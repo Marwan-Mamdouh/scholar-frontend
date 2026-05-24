ca34b791af2374b184a04e9b8938559cc8ff1884
marinaeleshaa <marinaeleshah77@gmail.com>
2026-05-24 23:43:19 +0300

    feat(Button): finalize Button implementation and remove deprecated files
    - Finished all button variants and edge cases handling
    - Cleaned up old button-related files and unused dependencies
    - Removed unused colors file

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,63 @@
+import { ReactNode, ButtonHTMLAttributes } from "react";
+import { ButtonIntent, ButtonSize, ButtonVariant } from "./button.type";
+import getButtonClasses, { getHoverOverlay } from "./button.style";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+  intent?: ButtonIntent;
+  size?: ButtonSize;
+  children?: ReactNode;
+  iconLeft?: ReactNode;
+  iconRight?: ReactNode;
+  textTransform?: "uppercase" | "lowercase" | "capitalize";
+}
+
+const Button = ({
+  variant = "solid",
+  intent = "primary",
+  size = "lg",
+  children = "icon",
+  iconLeft,
+  iconRight,
+  textTransform = "capitalize",
+  ...rest
+}: ButtonProps) => {
+  const overlay = getHoverOverlay(intent, variant);
+  return (
+    <button
+      {...rest}
+      className={`${getButtonClasses({ variant, intent, size })} ${textTransform}`}
+    >
+      <span className="relative z-10 flex items-center justify-center gap-2">
+        {iconLeft && (
+          <span className="group-hover:mr-2 transition-all ease-in-out duration-500">
+            {iconLeft}
+          </span>
+        )}
+        {children && (
+          <span className={`transition-all ease-in-out duration-500  `}>
+            {children}
+          </span>
+        )}
+        {iconRight && (
+          <span className="group-hover:ml-2 transition-all ease-in-out duration-500">
+            {iconRight}
+          </span>
+        )}
+      </span>
+
+      {/* a span for the overlay to make smooth hover effect */}
+      <span
+        className={`
+          absolute inset-0
+          opacity-0 group-hover:opacity-100
+          transition-all ease-in-out duration-500
+          pointer-events-none
+          bg-linear-to-b ${overlay}
+        `}
+      />
+    </button>
+  );
+};
+
+export default Button;
