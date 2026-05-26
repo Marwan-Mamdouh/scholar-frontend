ca70420da528a9a45b2079d8700b6a245e2965a0
marinaeleshaa <marinaeleshah77@gmail.com>
2026-05-26 23:22:26 +0300

    refactor(Home): remove unused Card and Badge components from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,4 @@
-import { Icon } from "@iconify/react";
-import Badge from "../components/ui/Badge/Badge";
-import Card from "../components/ui/Card/Card";
 
 export default function Home() {
-  return (
-    <main>
-      <Card
-        variant="solid"
-        intent="primary"
-        title="card title"
-        description="card description"
-        callToAction="call to action"
-        badge={
-          <Badge variant="outlined" intent="primary">
-            new badge
-          </Badge>
-        }
-        icon={<Icon className="h-full w-full" icon="fe:arrow-right" />}
-      />
-      <Card
-        variant="solid"
-        intent="primary"
-        title="card title"
-        description="card description"
-        callToAction="call to action"
-        badge={
-          <Badge variant="outlined" intent="primary">
-            new badge
-          </Badge>
-        }
-        clickable
-        icon={<Icon className="h-full w-full" icon="fe:arrow-right" />}
-      />
-    </main>
-  );
+  return <main></main>;
 }
