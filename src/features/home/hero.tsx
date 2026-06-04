aaf0d36c8d6e323a9811492501f5145237f864a8
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-06-04 21:48:11 +0300

    feat: add mobile view in the hero section component

--- src/features/home/hero.tsx
+++ src/features/home/hero.tsx
@@ -1,31 +1,43 @@
 import Button from "@/src/components/ui/Button/Button";
-import Image from "next/image";
+import LightingGlow from "@/src/components/ui/LightingGlow/LightingGlow";
 
 const HeroSection = () => {
   return (
-    <section className="relative w-full h-[715px] overflow-hidden bg-[#0B1A2B]">
-      {/* Background waves */}
+    <section className="relative overflow-hidden bg-[#05182A] w-full h-[520px] pt-6 pb-[100px] lg:h-[715px] lg:pt-0 lg:pb-0">
+      {/* Desktop SVG waves - hidden on mobile */}
       <img
         src="/hero_lines.svg"
         alt=""
         aria-hidden="true"
-        className="absolute inset-0 w-full h-full object-cover pointer-events-none"
+        className="hidden lg:block absolute inset-0 w-full h-full object-cover pointer-events-none"
       />
 
+      {/* Mobile glows - hidden on desktop */}
+      <div className="lg:hidden">
+        {/* Left glow */}
+        <div className="absolute bottom-78 -left-6 w-[136px] h-[65px]">
+          <LightingGlow variant="primary"></LightingGlow>
+        </div>
+        {/* Right glow */}
+        <div className="absolute bottom-67 -right-5 w-[147px] h-[67px]">
+          <LightingGlow variant="accent"></LightingGlow>
+        </div>
+      </div>
+
       {/* Text content */}
-      <div className="relative z-10 flex flex-col gap-[23px] w-[765px] top-[165px] left-[121px]">
+      <div className="relative z-10 flex flex-col px-5 lg:px-0 gap-[10px] w-full lg:gap-[23px] lg:w-[765px] lg:top-[165px] lg:left-[121px] lg:absolute">
         {/* Eyebrow */}
-        <p className="font-kanit font-normal text-[24px] leading-[50px] tracking-[0.20em] capitalize text-accent-400">
+        <p className="font-kadwa font-normal capitalize text-accent-400 text-[20px] leading-[50px] tracking-[0.20em] lg:text-[24px]">
           Scholar Nexus
         </p>
 
         {/* Headline */}
-        <h1 className="font-Kanit font-bold text-[48px] leading-[50px] tracking-[0.05em] capitalize text-neutral-50 w-[508px]">
+        <h1 className="font-kanit font-bold capitalize text-neutral-50 text-[40px] leading-[50px] tracking-[0.05em] w-full lg:text-[48px] lg:w-[508px]">
           Join To Connect And Advance Research
         </h1>
 
         {/* Body */}
-        <p className="font-kadwa font-normal text-[20px] leading-[30px] tracking-[0.05em] capitalize text-neutral-100 w-[765px]">
+        <p className="font-kadwa font-normal capitalize text-neutral-100 text-[16px] leading-[30px] tracking-[0.05em] w-full lg:text-[20px]">
           Bringing Academia And Industry Together. Connect, Explore Projects,
           And Unlock Opportunities.
         </p>
