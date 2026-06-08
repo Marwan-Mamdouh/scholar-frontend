8307578e8d90233627657419039c6efd424cb736
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-06-09 02:13:47 +0300

    style: meet some figma styles in the growth component and select the right color for the counter

--- src/features/home/StatCounter.tsx
+++ src/features/home/StatCounter.tsx
@@ -46,7 +46,7 @@ export default function StatCounter({
 
   return (
     <div className="flex flex-col items-center gap-2.5 w-30 lg:w-33.25">
-      <span className="font-[kanit] font-bold tracking-[0.05em] capitalize text-neutral-100 text-[32px] leading-7.5 lg:text-[40px]">
+      <span className="font-[kanit] font-bold tracking-[0.05em] capitalize text-#FFFFFF text-[32px] leading-7.5 lg:text-[40px]">
         {display}
       </span>
       <span className="font-[kanit] font-medium tracking-[0.05em] capitalize text-neutral-100 text-[20px] leading-7.5 lg:text-[24px]">
