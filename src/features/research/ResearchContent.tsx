96fa710aea17a8478e529cf0114474e4cdee3778
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-06-29 01:53:34 +0300

    refactpr: extend CONTENT_DATA with placeholder to use in the input placeholder and remove label prop form the input component as it is optinal now

--- src/features/research/ResearchContent.tsx
+++ src/features/research/ResearchContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { FC } from "react";
 import { Input } from "@/src/components/ui/InputField/Input";
 import dynamic from "next/dynamic";
 import notFoundAnimation from "@/src/components/assets/NotFound.json";
@@ -11,27 +11,35 @@ export interface ResearchContentProps {
 
 const CONTENT_DATA: Record<
   string,
-  { filters: string[]; emptyTitle: string; emptyDesc: string }
+  {
+    filters: string[];
+    emptyTitle: string;
+    emptyDesc: string;
+    placeholder: string;
+  }
 > = {
   researchers: {
     filters: ["Topics", "Research Fields", "Universities"],
     emptyTitle: "Find What You're Looking For",
     emptyDesc: "Choose Topics, Keywords, Or Universities To Begin Your Search.",
+    placeholder: "by Researcher Name",
   },
   papers: {
     filters: ["Year"],
     emptyTitle: "Start Your Paper Search",
     emptyDesc:
       "Search by keywords, paste an abstract, or filter by year to find the most relevant research",
+    placeholder: "Paper",
   },
   projects: {
     filters: ["Universities", "Project Field", "Year", "Sponsorship"],
     emptyTitle: "Find What You're Looking For",
     emptyDesc: "Choose topics, keywords, or universities to begin your search.",
+    placeholder: "by title, student or supervisor",
   },
 };
 
-const ResearchContent: React.FC<ResearchContentProps> = ({ activeTab }) => {
+const ResearchContent: FC<ResearchContentProps> = ({ activeTab }) => {
   const currentContent = CONTENT_DATA[activeTab] || CONTENT_DATA.researchers;
 
   return (
@@ -39,10 +47,7 @@ const ResearchContent: React.FC<ResearchContentProps> = ({ activeTab }) => {
       {/* Top action bar */}
       <div className="flex gap-4 items-center">
         <div className="w-75">
-          <Input
-            label="TEST"
-            placeholder={`Search by ${activeTab === "researchers" ? "Researcher" : activeTab === "papers" ? "Paper" : "Project"} Name`}
-          />
+          <Input placeholder={`Search ${currentContent.placeholder}`} />
         </div>
 
         {/* Dropdowns */}
@@ -70,9 +75,7 @@ const ResearchContent: React.FC<ResearchContentProps> = ({ activeTab }) => {
         <h3 className="text-2xl font-semibold text-accent-300">
           {currentContent.emptyTitle}
         </h3>
-        <p className="text-neutral-100">
-          {currentContent.emptyDesc}
-        </p>
+        <p className="text-neutral-100">{currentContent.emptyDesc}</p>
       </div>
     </div>
   );
