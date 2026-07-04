0b6b926ec6ffdcac4b3028e063f9a1a16dbfa334
Heba <hebaehab001@gmail.com>
2026-07-04 18:21:42 +0300

    refactor: arrange files component and some ui tailwind style and collect all types in one type file

--- src/features/about/Contact.tsx
+++ src/features/about/Contact.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import EmailFastOutlineIcon from "@iconify-react/mdi/email-fast-outline";
 import TwitterLineIcon from "@iconify-react/si/twitter-line";
-import OutlinePlaceIcon from "@iconify-react/ic/outline-place";
+// import OutlinePlaceIcon from "@iconify-react/ic/outline-place";
 import { Input } from "@/src/components/ui/InputField/Input";
 import Button from "@/src/components/ui/Button/Button";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
@@ -19,27 +19,24 @@ const contacts: ContactsList[] = [
     title: "@ScholarNexus",
     url: "",
   },
-  {
-    icon: OutlinePlaceIcon,
-    title: "Cairo, Egypt (Global HQ)",
-    url: "",
-  },
+  // {
+  //   icon: OutlinePlaceIcon,
+  //   title: "Cairo, Egypt (Global HQ)",
+  //   url: "",
+  // },
 ];
 
 export default function Contact() {
-  const {
-    control,
-    handleSubmit,
-  } = useForm({
+  const { control, handleSubmit } = useForm({
     defaultValues: {
       name: "",
       email: "",
       message: "",
     },
   });
-    const onSubmit: SubmitHandler<ContactData> = data => {
-        console.log(data)
-    };
+  const onSubmit: SubmitHandler<ContactData> = (data) => {
+    console.log(data);
+  };
   return (
     <section>
       <div className="rounded-3xl bg-neutral-50/5 shadow-[0px_2px_5px_0px_#70B5DF] py-10.5 px-6 lg:px-17.5 flex flex-col gap-6 lg:flex-row">
@@ -52,10 +49,10 @@ export default function Contact() {
             alt="email icon"
             className=" w-29 h-19.5 flex justify-self-center  md:justify-self-start"
           />
-          <h2 className="font-medium text-4xl leading-12.5 capitalize text-neutral-50">
+          <h2 className="font-medium text-4xl leading-12.5 capitalize text-neutral-50 text-center md:text-start">
             Start a Conversation
           </h2>
-          <p className="font-light text-base leading-6 lowercase text-neutral-100 lg:max-w-120">
+          <p className="font-light text-base leading-6 lowercase text-neutral-100 md:max-w-120 text-center md:text-start">
             Have questions about API integration, enterprise solutions, or
             removing your data? Our team is here to help.
           </p>
--- src/features/about/Pillars.tsx
+++ src/features/about/Pillars.tsx
@@ -1,18 +1,19 @@
-import React from "react";
 import Timeline from "./Timeline";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Pillars() {
   return (
     <div className="flex flex-col lg:flex-row-reverse items-center gap-2">
-      <DotLottieReact
-        src="https://lottie.host/3918952e-85a9-4a07-9846-0f8fea09beba/8cyikSTHfa.lottie"
-        loop
-        autoplay
-        className="w-220 h-90 lg:w-[50%] md:h-full relative md:translate-x-0"
-      />
+      <div className="w-full lg:w-1/2">
+        <DotLottieReact
+          src="https://lottie.host/3918952e-85a9-4a07-9846-0f8fea09beba/8cyikSTHfa.lottie"
+          loop
+          autoplay
+          className="w-220 h-90 md:h-fit relative -left-1/2 md:-left-1/10 lg:-left-1/4 top-0 "
+        />
+      </div>
       <div className="space-y-4 lg:w-[60%]">
-        <div className="tracking-wide">
+        <div className="text-center lg:text-start">
           <h2 className=" text-4xl lg:text-5xl capitalize font-semibold  leading-10 text-neutral-50 mb-2">
             What We're <span className="text-accent-400">Building</span>
           </h2>
