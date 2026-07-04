e4c2bca1c2c2a69873c9ddf2cec03a252b7f9998
Heba <hebaehab001@gmail.com>
2026-07-04 12:16:25 +0300

    feat:adding the accordin ui component and apply the members card on it to the meetourteam section

--- src/features/about/MeetOurTeam.tsx
+++ src/features/about/MeetOurTeam.tsx
@@ -1,14 +1,66 @@
+"use client";
+import Accordion from "@/src/components/ui/accordion/Accordion";
 import Button from "@/src/components/ui/Button/Button";
+import CodeIcon from "@iconify-react/tabler/code";
+import BookOutlineIcon from '@iconify-react/basil/book-outline';
+import IndustryIcon from '@iconify-react/cil/industry';
+import TeamCard from "./TeamCard";
+import { Member} from "./about.type";
+
+const team:Member[] = [
+  {
+    id: "1",
+    name: "heba",
+    role: "web",
+    linkedln: "linkedln.com",
+  },
+  {
+    id: "2",
+    name: "heba",
+    role: "web",
+    linkedln: "linkedln.com",
+  },
+  {
+    id: "3",
+    name: "heba",
+    role: "web",
+    linkedln: "linkedln.com",
+  },
+  {
+    id: "4",
+    name: "heba",
+    role: "web",
+    linkedln: "linkedln.com",
+  },
+  {
+    id: "5",
+    name: "heba",
+    role: "web",
+    linkedln: "linkedln.com",
+  },
+  {
+    id: "6",
+    name: "heba",
+    role: "web",
+    linkedln: "",
+  },
+  {
+    id: "7",
+    name: "heba",
+    role: "",
+  },
+];
 
 export default function MeetOurTeam() {
   return (
     <section>
-      <div className="flex flex-col lg:flex-row lg:justify-between gap-5">
+      {/* header */}
+      <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-5 py-8">
         <div className="tracking-wide">
           <span className="font-normal text-base leading-5 capitalize text-accent-300">
             The people behind it
           </span>
-          <h2 className=" text-4xl lg:text-5xl capitalize font-semibold  leading-10 text-neutral-50 mb-2">
+          <h2 className=" text-4xl lg:text-5xl capitalize font-semibold  leading-10 text-neutral-50 mb-4">
             Meet our <span className="text-accent-400">teams</span>
           </h2>
           <p className="text-neutral-100 text-lg max-w-2xl">
@@ -44,6 +96,30 @@ export default function MeetOurTeam() {
             </Button>
           </div>
         </div>
+      </div>
+      {/* tabs */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 ">
+        <Accordion title="web app team" subtitle={`${team.length} members`} icon={CodeIcon}>
+          <div className="space-y-4">
+            {team.map((member) => (
+              <TeamCard key={member.id} {...member}/>
+            ))}
+          </div>
+        </Accordion>
+        <Accordion title="Academia team" subtitle={`${team.length} members`} icon={BookOutlineIcon}>
+          <div className="space-y-4">
+            {team.map((member) => (
+              <TeamCard key={member.id} {...member}/>
+            ))}
+          </div>
+        </Accordion>
+        <Accordion title="Industry Team" subtitle={`${team.length} members`} icon={IndustryIcon}>
+          <div className="space-y-4">
+            {team.map((member) => (
+              <TeamCard key={member.id} {...member}/>
+            ))}
+          </div>
+        </Accordion>
       </div>
     </section>
   );
