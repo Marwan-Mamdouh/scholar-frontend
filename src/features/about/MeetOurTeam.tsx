e79c4e195f6c493ac9984a3a3ba466ab7d2a8952
Heba <hebaehab001@gmail.com>
2026-07-04 19:21:17 +0300

    refactor: handle the structure props and design according to the api comming data of trams in meetOurTeams section

--- src/features/about/MeetOurTeam.tsx
+++ src/features/about/MeetOurTeam.tsx
@@ -5,53 +5,16 @@ import CodeIcon from "@iconify-react/tabler/code";
 import BookOutlineIcon from "@iconify-react/basil/book-outline";
 import IndustryIcon from "@iconify-react/cil/industry";
 import TeamCard from "./TeamCard";
-import { Member } from "./about.type";
+import { MeetOurTeamProps } from "./about.type";
+import { ComponentType } from "react";
 
-const team: Member[] = [
-  {
-    id: "1",
-    name: "heba",
-    role: "web",
-    linkedln: "linkedln.com",
-  },
-  {
-    id: "2",
-    name: "heba",
-    role: "web",
-    linkedln: "linkedln.com",
-  },
-  {
-    id: "3",
-    name: "heba",
-    role: "web",
-    linkedln: "linkedln.com",
-  },
-  {
-    id: "4",
-    name: "heba",
-    role: "web",
-    linkedln: "linkedln.com",
-  },
-  {
-    id: "5",
-    name: "heba",
-    role: "web",
-    linkedln: "linkedln.com",
-  },
-  {
-    id: "6",
-    name: "heba",
-    role: "web",
-    linkedln: "",
-  },
-  {
-    id: "7",
-    name: "heba",
-    role: "",
-  },
+const teamsIcon: ComponentType<{ className?: string }>[] = [
+  CodeIcon,
+  BookOutlineIcon,
+  IndustryIcon,
 ];
 
-export default function MeetOurTeam() {
+export default function MeetOurTeam({ teams }: MeetOurTeamProps) {
   return (
     <section>
       {/* header */}
@@ -70,13 +33,17 @@ export default function MeetOurTeam() {
         </div>
         <div className="text-center grid grid-cols-2 gap-5">
           <div className="border border-accent-300 rounded-xl p-2.5 bg-linear-to-b from-primary-300/30 to-accent-400/10">
-            <p className="text-[32px] text-neutral-50 font-normal ">3</p>
+            <p className="text-[32px] text-neutral-50 font-normal ">
+              {teams.meta.teamCount}
+            </p>
             <p className="text-2xl text-accent-300 font-normal capitalize">
               Teams
             </p>
           </div>
           <div className="border border-accent-300 rounded-xl p-2.5 bg-linear-to-b from-primary-300/30 to-accent-400/10">
-            <p className="text-[32px] text-neutral-50 font-normal ">17</p>
+            <p className="text-[32px] text-neutral-50 font-normal ">
+              {teams.meta.memberCount}
+            </p>
             <p className="text-2xl text-accent-300 font-normal capitalize">
               Members
             </p>
@@ -96,39 +63,23 @@ export default function MeetOurTeam() {
       </div>
       {/* tabs */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 ">
-        <Accordion
-          title="web app team"
-          subtitle={`${team.length} members`}
-          icon={CodeIcon}
-        >
-          <div className="space-y-4">
-            {team.map((member) => (
-              <TeamCard key={member.id} {...member} />
-            ))}
-          </div>
-        </Accordion>
-        <Accordion
-          title="Academia team"
-          subtitle={`${team.length} members`}
-          icon={BookOutlineIcon}
-        >
-          <div className="space-y-4">
-            {team.map((member) => (
-              <TeamCard key={member.id} {...member} />
-            ))}
-          </div>
-        </Accordion>
-        <Accordion
-          title="Industry Team"
-          subtitle={`${team.length} members`}
-          icon={IndustryIcon}
-        >
-          <div className="space-y-4">
-            {team.map((member) => (
-              <TeamCard key={member.id} {...member} />
-            ))}
-          </div>
-        </Accordion>
+        {teams.data.map((team, index) => {
+          const teamIcon = teamsIcon[index];
+          return (
+            <Accordion
+              key={index}
+              title={team.title}
+              subtitle={`${team.count} members`}
+              icon={teamIcon}
+            >
+              <div className="space-y-4">
+                {team.members.map((member) => (
+                  <TeamCard key={member.id} {...member} />
+                ))}
+              </div>
+            </Accordion>
+          );
+        })}
       </div>
     </section>
   );
