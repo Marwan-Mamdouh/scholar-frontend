c4302e512d9aea75939d0217a54905b715c0e14c
Heba <hebaehab001@gmail.com>
2026-07-04 19:21:17 +0300

    refactor: handle the structure props and design according to the api comming data of trams in meetOurTeams section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,159 @@
+import { Member, TeamsData } from "@/src/features/about/about.type";
 import AboutTitle from "@/src/features/about/AboutTitle";
 import Contact from "@/src/features/about/Contact";
 import MeetOurTeam from "@/src/features/about/MeetOurTeam";
 import Pillars from "@/src/features/about/Pillars";
 import WhyNexus from "@/src/features/about/WhyNexus";
+
+const teams: TeamsData ={
+  meta:{
+    teamCount:3,
+    memberCount:17
+  },
+  data:[
+    {
+      title:"web app team",
+      count:7,
+      members: [
+        {
+          id: "1",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "2",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "3",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "4",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "5",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "6",
+          name: "heba",
+          role: "web",
+          linkedIn: "",
+        },
+        {
+          id: "7",
+          name: "heba",
+          role: "",
+        },
+      ]
+    },
+    {
+      title:"Academia team",
+      count: 7,
+      members: [
+        {
+          id: "1",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "2",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "3",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "4",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "5",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "6",
+          name: "heba",
+          role: "web",
+          linkedIn: "",
+        },
+        {
+          id: "7",
+          name: "heba",
+          role: "",
+        },
+      ]
+    },
+    {
+      title:"Industry Team",
+      count: 7,
+      members: [
+        {
+          id: "1",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "2",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "3",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "4",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "5",
+          name: "heba",
+          role: "web",
+          linkedIn: "linkedIn.com",
+        },
+        {
+          id: "6",
+          name: "heba",
+          role: "web",
+          linkedIn: "",
+        },
+        {
+          id: "7",
+          name: "heba",
+          role: "",
+        },
+      ]
+    },
+  ]
+}
 
 export default function AboutPage() {
   return (
@@ -10,7 +161,7 @@ export default function AboutPage() {
       <AboutTitle />
       <Pillars />
       <WhyNexus />
-      <MeetOurTeam />
+      <MeetOurTeam teams={teams}/>
       <Contact />
     </main>
   );
