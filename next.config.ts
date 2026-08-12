85088bfd9981745d001b8dae804b79cba9756fc0
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-08-12 03:48:24 +0300

    chore: ignore figma-api-key and allow LAN dev origin

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	// Next.js 16 specific optimizations
 	reactStrictMode: true,
-	reactCompiler: true,
+  reactCompiler: true,
+	allowedDevOrigins: ['192.168.100.200']
 };
 
 export default nextConfig;
